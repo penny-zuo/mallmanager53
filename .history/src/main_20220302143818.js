b737014cd32455be2330f5dc851009d71bc90554
penny-zuo <pennyzuo@yeah.net>
2022-03-02 14:44:35 +0800

    init

---
+++ .history/src/main_20220302143818.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+// 引入样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+//  适用于 vue 插件
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
